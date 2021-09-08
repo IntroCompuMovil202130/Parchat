fb303a92e05c1e75ae41250384f6c4cbca874c8f
Julian Tarazona <jdtarazona@javeriana.edu.co>
2021-09-07 23:53:46 -0500

    Iniciar sesión

---
+++ ParChat/app/src/main/java/com/cm4pks/parchat/iniciarsesion.java
@@ -0,0 +1,26 @@
+package com.cm4pks.parchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class iniciarsesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciarsesion);
+    }
+
+    public void registrarse(View v){
+        Intent intent = new Intent(v.getContext(),registrarse.class);
+        startActivity(intent);
+    }
+
+    public void recContrasena(View v){
+        Intent intent = new Intent(v.getContext(),recuperarClave.class);
+        startActivity(intent);
+    }
+}
