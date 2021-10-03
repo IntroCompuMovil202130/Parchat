85230bd6fa98c85470b74ad853c7d62ef84a124b
Fabian Rojas <fabian_rojas-10@hotmail.com>
2021-10-02 23:24:23 -0500

    Primera Entrega Actualizacion

---
+++ ParChat/app/src/main/java/com/example/parchat/Registrarse.java
@@ -0,0 +1,25 @@
+package com.example.parchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registrarse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+
+    public void iniciarSesion(View v){
+        Intent intent = new Intent(v.getContext(),MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void registrarse(View v){
+        //TODO Registrar Usuario Firebase Auth
+    }
+}
